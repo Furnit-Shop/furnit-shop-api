6b13e2de5b35a04bda3ad87883c691f03ce048de
Mai-Reaksa099 <ReaksaEngineering007@gmail.com>
2023-10-05 17:02:13 +0700

    modify booking

--- app/Http/Controllers/Booking/BookingController.php
+++ app/Http/Controllers/Booking/BookingController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Booking;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product\ProductModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BookingController extends Controller
 {
-    //
+    public function bookingProduct(){
+        $booking = DB::select('SELECT id FROM products');
+        if ($booking == 1){
+            return response([
+                'data'=>$booking
+            ]);
+        }
+        else if ($booking > 1){
+            return $booking;
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth;
 use  App\Http\Controllers\Product;
+use App\Http\Controllers\Booking;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,18 +15,20 @@ use  App\Http\Controllers\Product;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
 Route::get('/test', function (){
     return "Hello World";
 });
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::post('/register', [Auth\AuthUserController::class, 'register']);
 Route::get('/get_all', [Auth\AuthUserController::class, 'getAllUser']);
 Route::post('/login', [Auth\AuthUserController::class, 'login']);
 Route::get('/profile_user', [Auth\AuthUserController::class, 'profileUser']);
 
 Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('/get_product', [Booking\BookingController::class, 'bookingProduct']);
     Route::post('/product/post', [Product\ProductController::class, 'postProduct']);
     Route::get('/product/get_product', [Product\ProductController::class, 'getProduct']);
 });
