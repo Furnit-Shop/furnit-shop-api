0d95b5605d1b5cd6f84bd95c2c08e17cb49f6fe7
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2023-10-23 05:19:20 +0700

    defide feild user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name')->nullable();
+            $table->string('merchant')->nullable();
             $table->string('email')->unique();
             $table->string('phone_number');
+            $table->enum('user_role',['seller','costumer'])->default('seller');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
