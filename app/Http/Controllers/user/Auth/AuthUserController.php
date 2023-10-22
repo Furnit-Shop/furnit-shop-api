0aeb368b8ce6b313d446d87e108c15978a72837e
mai-reaska <2820.mai.reaksa@rupp.edu.kh>
2023-10-23 05:22:47 +0700

    modify controller

--- app/Http/Controllers/user/Auth/AuthUserController.php
+++ app/Http/Controllers/user/Auth/AuthUserController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class AuthUserSellerController extends Controller
+class AuthUserController extends Controller
 {
     public function register(Request $request){
         $request->validate([
